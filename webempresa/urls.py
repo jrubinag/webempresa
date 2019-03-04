cd720cd36972e79a685ffa1370e391ba5ca77621
Jonathan <jonathan_hrg@hotmail.com>
2019-03-04 16:50:20 -0500

    Solución: Tercera App [Services] Vista y template

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -21,6 +21,8 @@ from django.conf import settings
 urlpatterns = [
     # Paths del core
     path('', include('core.urls')),
+    # Paths del service
+    path('services/', include('services.urls')),
     # Paths del admin
     path('admin/', admin.site.urls),
 ]
